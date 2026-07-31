f93892ca19934cdee72912fe84b03e3537fd74c3
Eshan Reddy <eshan@reddymail.me>
2026-07-30 23:51:27 -0700

    Trying to prevent page zoom on safari

--- view/viewcontrols.js
+++ view/viewcontrols.js
@@ -34,6 +34,12 @@ class ViewControls  {
         this.display.canvas.addEventListener("touchstart", (event) => this.touchStart(event), {passive: false});
         this.display.canvas.addEventListener("touchmove", (event) => this.touchMove(event), {passive: false});
         this.display.canvas.addEventListener("touchend", (event) => this.touchEnd(event));
+        this.display.canvas.addEventListener("touchcancel", (event) => this.touchEnd(event));
+
+        // Preventing page zoom on safari
+        document.addEventListener("gesturestart", (event) => event.preventDefault());
+        document.addEventListener("gesturechange", (event) => event.preventDefault());
+        document.addEventListener("gestureend", (event) => event.preventDefault());
 
         addEventListener("wheel", (event) => this.wheel(event), {passive: false});
         addEventListener("resize", (event) => this.resetViewToDefault());
